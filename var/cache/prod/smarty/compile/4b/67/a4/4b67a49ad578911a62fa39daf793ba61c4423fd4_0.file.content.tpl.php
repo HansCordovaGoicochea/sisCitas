ce2ee2f6955bc19622abc83914ba342279902dc4
Ache <ache.cg@gmail.com>
2019-08-12 15:27:30 -0500

    first commit

---
+++ var/cache/prod/smarty/compile/4b/67/a4/4b67a49ad578911a62fa39daf793ba61c4423fd4_0.file.content.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-08-12 14:18:57
+  from 'C:\xampp\htdocs\siscitas\admincitas\themes\new-theme\template\content.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5d51bba1891ee1_65483001',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4b67a49ad578911a62fa39daf793ba61c4423fd4' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\siscitas\\admincitas\\themes\\new-theme\\template\\content.tpl',
+      1 => 1565636009,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d51bba1891ee1_65483001 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="ajax_confirmation" class="alert alert-success" style="display: none;"></div>
+
+
+<?php if (isset($_smarty_tpl->tpl_vars['content']->value)) {?>
+  <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+<?php }
+}
+}
---
+++ var/cache/prod/smarty/compile/e5/c9/05/e5c90520920ea29e123cb00a182c68acf400c009_0.file.footer.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-08-12 14:18:57
+  from 'C:\xampp\htdocs\siscitas\admincitas\themes\new-theme\template\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5d51bba1a30047_80412037',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e5c90520920ea29e123cb00a182c68acf400c009' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\siscitas\\admincitas\\themes\\new-theme\\template\\footer.tpl',
+      1 => 1565636009,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d51bba1a30047_80412037 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="footer" class="bootstrap">
+    <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['hook'][0], array( array('h'=>"displayBackOfficeFooter"),$_smarty_tpl ) );?>
+
+</div>
+<?php }
+}
